e80f87cbe50b2503f518798f672df1e250c6be2e
hrithik86 <hrithikmalik86@gmail.com>
2020-01-29 10:17:51 +0530

    numpy operations

---
+++ numpyOperations.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[2]:
+
+
+arr=np.arange(0,11)
+
+
+# In[3]:
+
+
+arr
+
+
+# In[4]:
+
+
+arr=arr*3
+arr
+
+
+# In[5]:
+
+
+arr+arr
+
+
+# In[7]:
+
+
+arr=arr/3
+
+
+# In[8]:
+
+
+arr
+
+
+# In[9]:
+
+
+arr*arr
+
+
+# In[10]:
+
+
+1/0
+
+
+# In[11]:
+
+
+arr/arr
+
+
+# In[12]:
+
+
+1/arr
+
+
+# In[13]:
+
+
+#universal array function
+np.sqrt(arr)
+
+
+# In[15]:
+
+
+arr.max()
+
+
+# In[16]:
+
+
+np.max(arr)
+
+
+# In[17]:
+
+
+np.exp(arr)
+
+
+# In[18]:
+
+
+np.sin(arr)
+
+
+# In[19]:
+
+
+np.log(arr)
+
